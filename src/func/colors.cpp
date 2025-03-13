701646744ea3c199b18a6e9a59b15d1d10d789ad
MatveySDK <matveywindows60@gmail.com>
2025-03-13 13:02:02 +0300

    Adding new  function to select single color

--- src/func/colors.cpp
+++ src/func/colors.cpp
@@ -13,12 +13,43 @@ const std::string MAGENTA = "\033[35m";
 const std::string CYAN = "\033[36m";
 const std::string WHITE = "\033[37m";
 
+enum ColorScheme {
+    RANDOM_COLOR,
+    RED_SCHEME,
+    GREEN_SCHEME,
+    YELLOW_SCHEME,
+    BLUE_SCHEME,
+    MAGENTA_SCHEME,
+    CYAN_SCHEME,
+    WHITE_SCHEME
+};
+
 bool useColors = true;
+ColorScheme colorScheme = RANDOM_COLOR;
 
 std::string getRandomColor() {
     const std::string colors[] = {RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
     
-    return colors[rand() % sizeof(colors)/sizeof(colors[0])];
+    switch(colorScheme) {
+        case RANDOM_COLOR:
+            return colors[rand() % sizeof(colors)/sizeof(colors[0])];
+        case RED_SCHEME:
+            return RED;
+        case GREEN_SCHEME:
+            return GREEN;
+        case YELLOW_SCHEME:
+            return YELLOW;
+        case BLUE_SCHEME:
+            return BLUE;
+        case MAGENTA_SCHEME:
+            return MAGENTA;
+        case CYAN_SCHEME:
+            return CYAN;
+        case WHITE_SCHEME:
+            return WHITE;
+        default:
+            return "";
+    }
 }
 
 void readConfigFile() {
@@ -36,14 +67,38 @@ void readConfigFile() {
                         useColors = false;
                     }
                 }
-                break;
+            }
+            if(line.find("$color_scheme") != std::string::npos) {
+                size_t pos = line.find('=');
+                if(pos != std::string::npos) {
+                    std::string value = line.substr(pos + 1);
+                    if(value == "random") {
+                        colorScheme = RANDOM_COLOR;
+                    } else if(value == "red") {
+                        colorScheme = RED_SCHEME;
+                    } else if(value == "green") {
+                        colorScheme = GREEN_SCHEME;
+                    } else if(value == "yellow") {
+                        colorScheme = YELLOW_SCHEME;
+                    } else if(value == "blue") {
+                        colorScheme = BLUE_SCHEME;
+                    } else if(value == "magenta") {
+                        colorScheme = MAGENTA_SCHEME;
+                    } else if(value == "cyan") {
+                        colorScheme = CYAN_SCHEME;
+                    } else if(value == "white") {
+                        colorScheme = WHITE_SCHEME;
+                    } else {
+                        colorScheme = RANDOM_COLOR; // По умолчанию случайный цвет
+                    }
+                }
             }
         }
-        configFile.close();  
+        configFile.close();
     } else {
-        std::cerr << "Cannot file open config.conf." << std::endl;
+        std::cerr << "$Error, file config.conf not found" << std::endl;
     }
-  }
+}
 
 void printAsciiArt(const std::string& asciiArt) {
     for(char c : asciiArt) {
@@ -60,4 +115,3 @@ void printAsciiArt(const std::string& asciiArt) {
         }
     }
 }
-
